a5a4288ef2c63e2eb4244675c90a2616bc352e20
rahulmmohan <rahulmohanpgr@gmail.com>
2018-01-15 20:38:14 +0530

    splash screen

---
+++ app/src/main/java/com/rahul/notely/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.rahul.notely
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by rahul on 15/01/18.
+ */
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // close this activity
+        finish()
+    }
+
+}
